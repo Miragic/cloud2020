94fb3995f9f02b004de5d77f12e22e3c4a98b980
xiangmingjie <469548329@qq.com>
2022-07-31 13:33:25 +0800

    learn-begin

---
+++ cloud-provider-payment8001/src/main/java/com/miragic/Payment8001Application.java
@@ -0,0 +1,11 @@
+package com.miragic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Payment8001Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001Application.class, args);
+    }
+}
